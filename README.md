5bfe9cbd69fcfc8eda122ba91a0d3951c4f4d093
datechie <umang.vanjara@gmail.com>
2018-02-03 15:55:33 -0800

    Initial files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# log-analytics
-Uacity FSND log analytics project
+# Udacity FSND Project - Log Analytics
+The project connects to multiple tables in the news Postgresql DB to produce reports. The project contains the following files:
+* **analysis.py**  - Python script to connect to the DB and print out the results
+* **README.md** -  This file which has details about the project and how to run
+* **sample-output.txt** - Contains the sample output 
+
+# Requirements
+* Python 2.7 (Not tested with other versions of Python)
+* Postgresql instance with the **news** database loaded
+
+# Install
+* Clone the repository using the command
+- `git clone https://github.com/datechie/log-analytics.git`
+
+# Setup
+Run the following commands while connected to the **news** database in the order specified below.
+
+1. For Query 1 
+- `create view titles as select split_part(path, '/', 3) as title, status, time, id from log where path LIKE '/article/%';`
+
+2. For Query 2
+- `create view author_titles as select b.author, count(*) as num from titles as a, articles as b where b.slug like a.title group by b.author order by num desc;`
+
+3. For Query 3
+- `create view date_requests_errors as select date(time) as error_view_date, count(*) as num, status from log where status not like '200%' group by error_view_date, status order by error_view_date asc;`
+
+- `create view date_requests_total as select date(time) as total_view_date, count(*) as num from log group by total_view_date order by total_view_date asc;`
+
+
+- `create view final as select a.error_view_date as request_date, a.num as errors, b.num as total, (a.num::float * 100)/(b.num::float) as percent from date_requests_errors as a, date_requests_total as b where a.error_view_date = b.total_view_date;`
+
+
+# How to run the Script
+The script can be run with either command from the below list:
+- `python analysis.py`
+- `python2.7 analyis.py`
+
+
+# Output
+- The included sample-output.txt file shows the example results.
---
+++ analysis.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python2.7
+# Udacity FSND Log Analytics Project
+
+# import modules that we need
+import datetime
+import psycopg2
+import time
+
+DBNAME = "news"  # Our database name
+
+db = psycopg2.connect(database=DBNAME)
+
+# Query for Part 1
+print ("Running query to get the 3 most popular articles of all time...")
+start1 = time.time()
+c1 = db.cursor()
+c1.execute("select a.title, count(*) as num from titles as a, articles as b" +
+           " where b.slug like a.title group by a.title" +
+           " order by num desc limit 3")
+# c1.execute("select title, num from titles2 order by num desc limit 3")
+rows1 = c1.fetchall()
+print ('...done in %.2f seconds' % (time.time() - start1))
+
+# Query for Part 2
+print ("Running query to get the most popular authors ...")
+start2 = time.time()
+c2 = db.cursor()
+c2.execute("select a.name, b.num from author_titles as b, authors as a where" +
+           " b.author = a.id")
+rows2 = c2.fetchall()
+print ('...done in %.2f seconds' % (time.time() - start2))
+
+# Query for Part 3
+print ("Running query to find days with > 1% of requests with errors...")
+start3 = time.time()
+c3 = db.cursor()
+c3.execute("select request_date, percent from final where percent > 1")
+rows3 = c3.fetchall()
+print ('...done in %.2f seconds' % (time.time() - start3))
+
+# Queries done, close the DB connection
+db.close()
+
+# Now we print the results
+print ("\nWhat are the most popular three articles of all time?")
+for a in range(len(rows1)):
+    print (" * {} - {} views").format(rows1[a][0], rows1[a][1])
+
+print ("\nWho are the most popular article authors of all time?")
+for b in range(len(rows2)):
+    print (" * {} - {} views").format(rows2[b][0], rows2[b][1])
+
+print ("\nOn which days did more than 1% of requests lead to errors?")
+for c in range(len(rows3)):
+    # Change the date format to be like July 2, 2018
+    request_date = rows3[c][0].strftime("%B %d, %Y")
+    # Rounding the percentage to two decimals
+    percent = round(rows3[c][1], 2)
+    print (" * {} - {}% errors").format(request_date, percent)
